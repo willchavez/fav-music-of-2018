c218fa5a21569907bdb0b331fcd99cea2ec01ce6
Glitch (fav-music-of-2018) <none>
2018-12-13 17:31:05 +0000

    🙀🌾 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,22 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
